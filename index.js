666ec35899dc1ed766aa8589f7b028799bab734c
ndizeyedavid <davidndizeye101@gmail.com>
2024-08-06 12:47:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql2'
+
+const port = 9090; // now don't judge me, i was just testing lol😁
+const app = express();
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "esp"
+})
+
+
+app.get('/data', (req, res)=>{
+    const sql = "SELECT * FROM SensorData";
+    db.query(sql, (err, data)=>{
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/average', (req, res)=>{
+    const sql = "SELECT ROUND(AVG(value1), 1) as value1, ROUND(AVG(value2), 1) as value2, ROUND(AVG(value3), 1) as value3 FROM sensordata;"
+    db.query(sql, (err, data)=>{
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+});
+
+app.listen(port, ()=>{
+    console.log('app running on port ', port)
+})
