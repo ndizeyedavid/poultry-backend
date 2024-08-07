ef566b6f710dfc6f35148867454f16b098ac0145
ndizeyedavid <davidndizeye101@gmail.com>
2024-08-07 13:07:17 +0300

    #33 commit

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ console.log('Databse connected successfuly')
 app.get('/data', (req, res)=>{
     const sql = "SELECT * FROM SensorData";
     db.query(sql, (err, data)=>{
-        if (err) return res.json(err);
+        if (err) return console.log(err);
         return res.json(data);
     })
 })
 
 app.get('/average', (req, res)=>{
-    const sql = "SELECT ROUND(AVG(value1), 1) as value1, ROUND(AVG(value2), 1) as value2, ROUND(AVG(value3), 1) as value3 FROM sensordata;"
+    const sql = "SELECT ROUsND(AVG(value1), 1) as value1, ROUND(AVG(value2), 1) as value2, ROUND(AVG(value3), 1) as value3 FROM sensordata;"
     db.query(sql, (err, data)=>{
         if (err) return res.json(err);
         return res.json(data);
