b882af82d0b04e0453e5735cb04b294ccedcf767
ndizeyedavid <davidndizeye101@gmail.com>
2025-02-04 18:14:34 +0200

    New changes

--- insert.js
+++ insert.js
@@ -1,32 +1,32 @@
 // const express = require('express');
-import mysql from 'mysql2';
+import mysql from "mysql2";
 // const cors = require('cors');
 
-
 const db = mysql.createConnection({
-    host: "i52.h.filess.io",
-    user: "testdb12234_distancehe",
-    password: "74ab35df9812ba304bd85706df45ae06f9746142",
-    database: "testdb12234_distancehe",
-    port: "3305"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "poultry",
 });
-db.on('err', (err) => {
-    console.log('Database connection failed');
+db.on("err", (err) => {
+  console.log("Database connection failed");
 });
 
-db.on('connect', (e)=>{
-    console.log('Databse connected');
-})
+db.on("connect", (e) => {
+  console.log("Databse connected");
+});
 
-function insertThis(){
-    const sql = `
-    INSERT INTO sensordata(temperature, humidity, ammonia) 
-    VALUES('${Math.floor(Math.random() * 80)}', '${Math.floor(Math.random() * 80)}', '${Math.floor(Math.random() * 80)}')
+function insertThis() {
+  const sql = `
+    INSERT INTO tbl_temperature(temperature, humidity, gaz) 
+    VALUES('${Math.floor(Math.random() * 80)}', '${Math.floor(
+    Math.random() * 80
+  )}', '${Math.floor(Math.random() * 80)}')
     `;
-    db.query(sql, (err, data)=>{
-        if (err) return console.log(err);
-        console.log("Data Inserted");
-        // return res.json(data);
-    });
+  db.query(sql, (err, data) => {
+    if (err) return console.log(err);
+    console.log("Data Inserted");
+    // return res.json(data);
+  });
 }
 setInterval(insertThis, 1500);
