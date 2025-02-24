78005fffde824cb997d3b7aa044c2589b3b5d2c1
ndizeyedavid <davidndizeye101@gmail.com>
2025-02-24 18:32:41 +0200

    added json parsing

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import mysql from "mysql2";
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const port = process.env.PORT;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log("server running on: " + port);
